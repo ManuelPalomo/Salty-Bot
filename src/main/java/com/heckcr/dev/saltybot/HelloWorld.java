a0c380399650de7542af9258351b8aaf0eb4497e
Manuel Palomo Ramon <palomoraising@gmail.com>
2016-03-15 21:22:18 +0100

    Added wrappers for dependencies

--- src/main/java/com/heckcr/dev/saltybot/HelloWorld.java
+++ src/main/java/com/heckcr/dev/saltybot/HelloWorld.java
@@ -1,7 +1,18 @@
 package com.heckcr.dev.saltybot;
 
+import com.heckcr.dev.wrappers.tts.TextToSpeech;
+import com.sun.speech.freetts.Voice;
+import com.sun.speech.freetts.VoiceManager;
+
 public class HelloWorld {
 	public static void main (String [] args){
-		System.out.println("Banana");
+		TextToSpeech tts = new TextToSpeech("kevin");
+	
+		tts.speakNonBlocking("Non Blocking");
+		tts.speakBlocking("Blocking");
+		
+		
+		
+		
 	}
 }
