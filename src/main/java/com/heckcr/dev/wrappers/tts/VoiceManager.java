1e0c1a2a5fdca07d6fc44a5250812b9d3b945b54
Manuel Palomo Ramon <palomoraising@gmail.com>
2016-03-18 16:24:44 +0100

    Added comments to the TTS main class

--- src/main/java/com/heckcr/dev/wrappers/tts/VoiceManager.java
+++ src/main/java/com/heckcr/dev/wrappers/tts/VoiceManager.java
@@ -1,5 +1,10 @@
 package com.heckcr.dev.wrappers.tts;
 
+/**
+ * Class that handles all the Text to Speech system using FreeTTS Allows to
+ * create blocking and non blocking voices
+ *
+ */
 public class VoiceManager {
 	private VoiceSupplier supplier;
 
@@ -7,14 +12,32 @@ public class VoiceManager {
 		this.supplier = VoiceSupplier.getInstance();
 	}
 
+	/**
+	 * Creates a blocking voice
+	 * 
+	 * @param text
+	 *            Text that is going to be spoken by the voice
+	 * @param voiceName
+	 *            Name of the voice that's going to be used, the voice includes
+	 *            the style and the language
+	 */
 	public void speakBlocking(String text, String voiceName) {
 		VoiceWrapper voice = supplier.getVoice(voiceName);
 		voice.getVoice().allocate();
 		voice.getVoice().speak(text);
 		supplier.notifyFree(voice);
-		
+
 	}
 
+	/**
+	 * Creates a non blocking voice using a thread
+	 * 
+	 * @param text
+	 *            Text that is going to be spoken by the voice
+	 * @param voiceName
+	 *            Name of the voice that's going to be used, the voice includes
+	 *            the style and the language
+	 */
 	public void speakNonBlocking(String text, String voiceName) {
 		VoiceWrapper voice = supplier.getVoice(voiceName);
 		VoiceThread threadVoice = new VoiceThread(voice, text);
