ea784ecc1e148ef8fdbce90fa7ac3ed5e543b328
Carlos Pinedo <carlos.pinedop@gmail.com>
2016-03-18 16:32:13 +0100

    Created an example of use in the main class

--- src/main/java/com/heckcr/dev/saltybot/HelloWorld.java
+++ src/main/java/com/heckcr/dev/saltybot/HelloWorld.java
@@ -4,9 +4,11 @@ import com.heckcr.dev.wrappers.tts.VoiceManager;
 
 public class HelloWorld {
 	public static void main (String [] args){
-		
-			
-		
-		
+            VoiceManager manager = new VoiceManager();
+            
+            manager.speakBlocking("Welcome to the tavern!", "kevin");
+            
+            manager.speakNonBlocking("Yes, i used to do it", "kevin");
+            manager.speakNonBlocking("A pint of beer please", "kevin");
 	}
 }
